cbf746a89f43ebd73272a0745811e776cd41d4e9
TAMIKA FILEDS <tjfields14@gmail.com>
2025-08-11 11:29:28 +0000

    static export build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'export'   // tells Next to generate static HTML in /out
-};
-module.exports = nextConfig;
+module.exports = { reactStrictMode: true, output: 'export' };
